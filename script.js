8892a09cd494ce700e6d05c893af32b4e23c4ae8
Alonzo Yrigollen <geekwise.alonzo.yrigollen@gmail.com>
2016-04-25 18:15:17 -0700

    w
    s

--- script.js
+++ script.js
@@ -1,75 +1,57 @@
-var square;
-var up_key = 38;
-var down_key = 40;
-var left_key = 37;
-var right_key = 39;
-var square_left;
-var square_top;
+var numLength = 0;
+function createTableElement(numCols, numRows) {
+  var table = document.createElement('table');
+  for (var r = 0; r < numRows; r++) {
+      var row = document.createElement('tr');
+      row.className += ' row';
+      for (var c = 0; c < numCols; c++) {
+          var cell = document.createElement('td');
+          cell.className += ' cell';
+          row.appendChild(cell);
+          numLength++;
+      }
+      table.appendChild(row);
+  }
+  return table;
+}
+//shift 16 cntrl 17 mouse left 0 mouse right 2
+function addClass(element, classname) {
+element.classname += ' ' + classname;
+}
 
-document.addEventListener('DOMContentLoaded',function(event){
-    
-    
-    
-    
-    
-    console.log('browser loaded');
-    
-    square = document.createElement('div');
-    square.style.width = '30px';
-    square.style.height = '30px';
-    square.style.backgroundColor = 'aqua';
-    square.style.position = 'relative';
-    square.style.left = '0px';
-    square.style.top = '0px';
-        
-    
-    square_left = function(){
-        return  parseInt(square.style.left);
-    };
-        
-    
-    square_top = function(){
-        return  parseInt(square.style.top);   
-    };
-    
-    document.body.appendChild(square);
-    
-    document.body.onkeydown = function(event){
+function addConsoleLogOnClickEvent(element, number) {
+  element.onclick = function () {
+      console.log("CLICKED");
+  };
+}
 
-        console.log(event.keyCode);
-        
-        if(event.keyCode == right_key){
-            console.log('right key was pressed');
-            
-            square.style.left = ( square_left()+10 ).toString() + 'px';      
-            
-        };
-        
-        
-        if(event.keyCode == left_key){
-            
-            console.log('left key was pressed');
-            
-            square.style.left = ( square_left()-10 ).toString() + 'px';
-            
-        }
-        
-        
-        
-        if(event.keyCode == down_key){
-            square.style.top = ( square_top()+10 ).toString() + 'px';
-        };
-        
-        if(event.keyCode == up_key){
-            square.style.top = ( square_top()-10 ).toString() + 'px';
-        }
-        
-        
-        
-        
-        
-    };
+function addEvent(table) {
+var cells = table.getElementsByTagName('td');
+for(var i = 0; i < numLength; i++) {
+  addConsoleLogOnClickEvent(this);
+  cells[i].onclick = function () {
+    if(event.ctrlKey){
+      this.style.backgroundColor = 'green';
+    }
+    else if(event.shiftKey){
+      this.style.backgroundColor = 'blue';
+    }
+    else if(event.altKey){
+      this.style.backgroundColor = '#fff';
+    }
+    else this.style.backgroundColor = 'red';
+  };
+}
+}
+var theGrid = document.getElementById("the-grid");
 
-    
-    
-});
+var theGridTable = createTableElement(40,30);
+
+document.body.appendChild(the-grid);
+
+addEvent(theGridTable);
+
+for(var i = 0; i < numLength; i++) {
+document.getElementsByTagName("td")[i].style.padding = "10px";
+document.getElementsByTagName("td")[i].style.border = "1px solid black";
+}
